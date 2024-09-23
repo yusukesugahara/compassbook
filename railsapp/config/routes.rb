7b423d4f24971de673515da98dbeda4deb1e0bae
yusukesugahara <yusuke.sugahara@gmail.com>
2024-09-23 19:49:40 +0900

    headerの作成、ログイン機能viewの編集

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +11,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  devise_for :users
+  root 'home#index'
+
 end
