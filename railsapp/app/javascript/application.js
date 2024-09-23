12eb125d12517784860d191104e64e687b192cb8
yusukesugahara <yusuke.sugahara@gmail.com>
2024-09-23 17:51:10 +0900

    device インストール

--- railsapp/app/javascript/application.js
+++ railsapp/app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import '@fortawesome/fontawesome-free'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(fas, far, fab)
