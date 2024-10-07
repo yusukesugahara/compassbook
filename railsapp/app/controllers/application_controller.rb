2bfcbe4f65e1fa2c6a53bbab9fa92240502c2f0c
yusukesugahara <yusuke.sugahara@gmail.com>
2024-10-07 15:09:11 +0900

    サインアップ時とユーザー情報変更時、nameの入力欄を追加

--- railsapp/app/controllers/application_controller.rb
+++ railsapp/app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  class ApplicationController < ActionController::Base
-    def after_sign_in_path_for(resource)
-      userpage_path  # ログイン後に userpage へリダイレクト
-    end
+
+  # Deviseのパラメータ許可を設定
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    userpage_path  # ログイン後に userpage へリダイレクト
   end
-  
+
+  protected
+
+  # サインアップ時とアカウント更新時に:nameパラメータを許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
--- railsapp/app/models/user.rb
+++ railsapp/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :projects, dependent: :destroy
+
+  validates :name, presence: true
+
 end
